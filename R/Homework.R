9d044f5869dee27b1c7503b9106b4d52d5ba24e8
Panustc <745036240@qq.com>
2024-12-08 15:41:08 +0800

    Updated DESCRIPTION, NAMESPACE, and Homework files

--- R/Homework.R
+++ R/Homework.R
@@ -1,6 +1,13 @@
 #' @import microbenchmark
 #' @import knitr
+#' @import xtable
+#' @import MASS
+#' @import extraDistr
+#' @import ggplot2
 #' @import boot
-#' @import pracma
 #' @import stats
 #' @import Rcpp
+#' @title Required R Packages
+#' @name Required R Packages
+#' @description The following R packages are required for running the examples and analyses in the vignette: microbenchmark ,Rcpp,ggplot2,xtable,boot,stats
+NULL
